e18673b50679677b00dcaf1ec07d24309ee0f1f1
RRCRis <curiosity.cdrr@gmail.com>
2022-08-03 17:15:42 -0500

    nuevo readme

--- PI-Videogames-main/api/src/app.js
+++ PI-Videogames-main/api/src/app.js
@@ -8,8 +8,6 @@ require("./db.js");
 
 const server = express();
 
-server.name = "API";
-
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
